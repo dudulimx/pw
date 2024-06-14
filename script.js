4e3b71321ebeacd54c89367feb3588288dce8995
Carlos Eduardo <128180987+dudulimx@users.noreply.github.com>
2024-06-14 07:43:01 -0300

    Site pomodoro com HTML ,CSS, JS

---
+++ script.js
@@ -0,0 +1,108 @@
+let focusButton = document.getElementById("pomodoro");
+let buttons = document.querySelectorAll(".btn");
+let shortBreakButton = document.getElementById("intvcurto");
+let longBreakButton = document.getElementById("intvlongo");
+let startBtn = document.getElementById("btn-iniciar");
+let reset = document.getElementById("btn-resetar");
+let pause = document.getElementById("btn-pausar");
+let time = document.getElementById("tempo");
+let set;
+let active = "pomodoro";
+let count = 59;
+let paused = true;
+let minCount = 24;
+time.textContent = `${minCount + 1}:00`;
+
+const appendZero = (value) => {
+  value = value < 10 ? `0${value}` : value;
+  return value;
+};
+
+reset.addEventListener(
+  "click",
+  (resetTime = () => {
+    pauseTimer();
+    switch (active) {
+      case "long":
+        minCount = 14;
+        break;
+      case "short":
+        minCount = 4;
+        break;
+      default:
+        minCount = 24;
+        break;
+    }
+    count = 59;
+    time.textContent = `${minCount + 1}:00`;
+  })
+);
+
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-pomodoro");
+  });
+};
+
+focusButton.addEventListener("click", () => {
+  removeFocus();
+  focusButton.classList.add("btn-pomodoro");
+  pauseTimer();
+  minCount = 24;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+shortBreakButton.addEventListener("click", () => {
+  active = "short";
+  removeFocus();
+  shortBreakButton.classList.add("btn-pomodoro");
+  pauseTimer();
+  minCount = 4;
+  count = 59;
+  time.textContent = `${appendZero(minCount + 1)}:00`;
+});
+
+longBreakButton.addEventListener("click", () => {
+  active = "long";
+  removeFocus();
+  longBreakButton.classList.add("btn-pomodoro");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+pause.addEventListener(
+  "click",
+  (pauseTimer = () => {
+    paused = true;
+    clearInterval(set);
+    startBtn.classList.remove("oculto");
+    pause.classList.remove("mostrar");
+    reset.classList.remove("mostrar");
+  })
+);
+
+startBtn.addEventListener("click", () => {
+  reset.classList.add("mostrar");
+  pause.classList.add("mostrar");
+  startBtn.classList.add("oculto");
+  startBtn.classList.remove("mostrar");
+  if (paused) {
+    paused = false;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    set = setInterval(() => {
+      count--;
+      time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+      if (count == 0) {
+        if (minCount != 0) {
+          minCount--;
+          count = 60;
+        } else {
+          clearInterval(set);
+        }
+      }
+    }, 1000);
+  }
+});
